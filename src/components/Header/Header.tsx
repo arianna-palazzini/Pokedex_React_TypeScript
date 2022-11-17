8c129d6c4610dc7cab325b42aaae49a3cbfdd471
ari_plz <98736077+arianna-palazzini@users.noreply.github.com>
2022-11-17 17:44:41 +0100

    Add files via upload

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,42 +1,43 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { StateI } from '../../store/reducer';
-import './Header.css';
-import { NavLink } from 'react-router-dom';
-import { RootState } from '../../store/reducer/index';
-import { Navigate } from 'react-router-dom';
-import { logout } from '../../store/actionCreators';
+import { useDispatch, useSelector } from "react-redux";
+import "./Header.css";
+import { NavLink } from "react-router-dom";
+import { RootState } from "../../store/reducer/index";
+import { Navigate } from "react-router-dom";
+import { logout } from "../../store/actionCreators";
 
+function Header() {
+  const user = useSelector((state: RootState) => state.general.user);
+  const dispatch = useDispatch();
 
-function Header(){
+  function eliminaUser() {
+    dispatch(logout());
+    sessionStorage.removeItem("cred");
+    <Navigate to="/" />;
+  }
 
-    const user = useSelector((state: RootState) => state.general.user)
-    const dispatch = useDispatch();
-
-    function eliminaUser(){
-        dispatch(logout());
-        sessionStorage.removeItem('cred');
-        <Navigate to='/'/>
-    }
-
-    return(
-        <div id='header'>
-            <h2>Pokedex</h2>
-            {
-            user ?
-            <>
-            <button id='logout' onClick={eliminaUser}>Logout</button>
-            </>
-            :
-            <NavLink to='/login'
-            style={({ isActive }) => ({
-                textDecoration: "none",
-                color: isActive ? "white" : "yellow",
-                fontSize: "large"
-            })}
-            >Login</NavLink>
-            }
-        </div>
-    )
+  return (
+    <div id="header">
+      <h2>Pokedex</h2>
+      {user ? (
+        <>
+          <button id="logout" onClick={eliminaUser}>
+            Logout
+          </button>
+        </>
+      ) : (
+        <NavLink
+          to="/login"
+          style={({ isActive }) => ({
+            textDecoration: "none",
+            color: isActive ? "white" : "yellow",
+            fontSize: "large",
+          })}
+        >
+          Login
+        </NavLink>
+      )}
+    </div>
+  );
 }
 
 export default Header;
--- src/pages/ListaPokemon/ListaPokemon.tsx
+++ src/pages/ListaPokemon/ListaPokemon.tsx
@@ -1,73 +1,72 @@
-import './ListaPokemon.css';
-import { NavLink, useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { chiamaPokemon } from '../../store/actionCreators';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../store/reducer/index';
-import { StateI } from '../../store/reducer';
+import "./ListaPokemon.css";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { chiamaPokemon } from "../../store/actionCreators";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/reducer/index";
 
 export interface Pokemon {
-    name: string,
-    url: string
+  name: string;
+  url: string;
 }
 
 function ListaPokemon() {
+  const listaPokemon: Pokemon[] = useSelector(
+    (state: RootState) => state.general.pokemon
+  );
+  const user = useSelector((state: RootState) => state.general.user);
+  const dispatch = useDispatch<any>();
+  const navigate = useNavigate();
 
-    const listaPokemon: Pokemon[] = useSelector((state: RootState) => state.general.pokemon);
-    const user = useSelector((state: RootState) => state.general.user)
-    const dispatch = useDispatch<any>();
-    const navigate = useNavigate();
+  useEffect(() => {
+    dispatch(chiamaPokemon());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    console.log(listaPokemon);
+  function viewDetail(nomePokemon: string) {
+    navigate(`/pokemon/${nomePokemon}`);
+  }
 
-
-    useEffect(() => {
-        dispatch(chiamaPokemon());
-    }, [])
-
-    function viewDetail(nomePokemon: string) {
-        navigate(`/pokemon/${nomePokemon}`);
-    }
-
-    return (
+  return (
+    <>
+      {user ? (
         <>
-            {
-                user ?
-                    <>
-                        <h2> Lista Pokemon</h2>
-                        <div id='lista-pokemon'>
-
-                            {
-                                listaPokemon.map(el =>
-                                    <button className='pokemon-in-lista' key={el.url} onClick={() => viewDetail(el.name)}>
-                                        {el.name}
-                                    </button>
-                                )
-                            }
-
-                        </div>
-                        <div id='div-crea'>
-                            <NavLink to={'/nuovo-pokemon'}
-                                style={({ isActive }) => ({
-                                    textDecoration: "none",
-                                    fontSize: "large",
-                                    color: "rgb(17, 30, 103)",
-                                    backgroundColor: "yellow",
-                                    padding: "8px",
-                                    borderRadius: "5px"
-                                })}>
-                                Crea nuovo Pokemon</NavLink>
-                        </div>
-                    </>
-                    :
-                    <>
-                        <div id='lista-pokemon'>
-                        </div>
-                    </>
-            }
+          <h2> Lista Pokemon</h2>
+          <div id="lista-pokemon">
+            {listaPokemon.map((el) => (
+              <button
+                className="pokemon-in-lista"
+                key={el.url}
+                onClick={() => viewDetail(el.name)}
+              >
+                {el.name}
+              </button>
+            ))}
+          </div>
+          <div id="div-crea">
+            <NavLink
+              to={"/nuovo-pokemon"}
+              style={({ isActive }) => ({
+                textDecoration: "none",
+                fontSize: "large",
+                color: "rgb(17, 30, 103)",
+                backgroundColor: "yellow",
+                padding: "8px",
+                borderRadius: "5px",
+              })}
+            >
+              Crea nuovo Pokemon
+            </NavLink>
+          </div>
         </>
-    )
+      ) : (
+        <>
+          <div id="lista-pokemon"></div>
+        </>
+      )}
+    </>
+  );
 }
 
 export default ListaPokemon;
